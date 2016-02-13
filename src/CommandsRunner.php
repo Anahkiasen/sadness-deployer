b8d0bedc0d3a201f3689aab62ac60c305a0e5beb
Maxime Fabre <ehtnam6@gmail.com>
2016-02-13 16:03:43 +0100

    Add classes

---
+++ src/CommandsRunner.php
@@ -0,0 +1,124 @@
+<?php
+namespace SadnessDeployer;
+
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+class CommandsRunner
+{
+    /**
+     * @var array
+     */
+    protected $output = [];
+
+    /**
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * @param string $basePath
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = $basePath;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////// RUNNING ///////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * @param array $commands
+     *
+     * @return array
+     */
+    public function runCommands(array $commands)
+    {
+        foreach ($commands as $command) {
+            $process = $this->run($command);
+            if (!$process->status) {
+                break;
+            }
+        }
+
+        return $this->output;
+    }
+
+    /**
+     * @param string|array $command
+     *
+     * @return object
+     */
+    public function run($command)
+    {
+        // Build process
+        $sanitized = $this->sanitizeCommand($command);
+        $process   = new Process($sanitized, $this->basePath);
+
+        // Run process
+        $output = '';
+        $process->run(function ($type, $buffer) use (&$output) {
+            $output .= $buffer;
+        });
+
+        // Wait for process
+        while ($process->isRunning()) {
+            // ...
+        }
+
+        // Sanitize output
+        $output = $output ?: '';
+        $output = trim($output, PHP_EOL).PHP_EOL;
+        $status = $process->getExitCode() === 0;
+
+        // Register command
+        $process = (object) [
+            'command'   => $command,
+            'sanitized' => $process->getCommandLine(),
+            'output'    => $output,
+            'status'    => $status,
+        ];
+
+        $this->output[] = $process;
+
+        return $process;
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////// HELPERS ///////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * @param string|array $command
+     *
+     * @return string
+     */
+    protected function sanitizeCommand($command)
+    {
+        $php = (new PhpExecutableFinder())->find(false);
+
+        $command = is_array($command) ? implode(' ', $command) : $command;
+        $command = str_replace('composer ', 'COMPOSER_HOME='.storage_path('composer').' php composer.phar ', $command);
+        $command = str_replace('artisan ', 'php artisan ', $command);
+        $command = str_replace('php ', $php.' ', $command);
+
+        return $command;
+    }
+}
---
+++ src/Deployer.php
@@ -0,0 +1,163 @@
+<?php
+namespace SadnessDeployer;
+
+use Closure;
+
+class Deployer
+{
+    /**
+     * @var CommandsRunner
+     */
+    protected $runner;
+
+    /**
+     * @param CommandsRunner $runner
+     */
+    public function __construct(CommandsRunner $runner)
+    {
+        $this->runner = $runner;
+        $this->runner->setBasePath(base_path());
+    }
+
+    /**
+     * Deploy the application
+     *
+     * @param Closure|null $callback
+     *
+     * @return array
+     */
+    public function deploy(Closure $callback = null)
+    {
+        $callback = $callback ?: function (self $runner) {
+            $runner->run('artisan down');
+            $runner->repository();
+            $runner->environment();
+            $runner->dependencies();
+            $runner->clear();
+            $runner->annotations();
+            $runner->optimize();
+            $runner->database();
+            $runner->run('artisan up');
+        };
+
+        $callback($this);
+
+        return $this->runner->getOutput();
+    }
+
+    /**
+     * @param string|array $commands
+     */
+    public function run($commands)
+    {
+        $this->runner->runCommands((array) $commands);
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    /////////////////////////////// TASKS ////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * Update repository
+     */
+    protected function repository()
+    {
+        $this->runner->runCommands([
+            'git reset --hard',
+            'git clean -df',
+            'git pull',
+        ]);
+    }
+
+    /**
+     * Copy environment file
+     *
+     * @param string $from
+     */
+    protected function environment($from = 'staging')
+    {
+        $this->runner->runCommands([
+            'rm .env',
+            'cp .env.' .$from. ' .env',
+        ]);
+    }
+
+    /**
+     * Update dependencies
+     */
+    protected function dependencies()
+    {
+        if (!file_exists($this->runner->getBasePath().'/composer.phar')) {
+            $this->getComposer();
+        }
+
+        $this->runner->runCommands([
+            'composer self-update',
+            'composer update --no-scripts',
+        ]);
+    }
+
+    /**
+     * Clear various caches
+     */
+    protected function clear()
+    {
+        $this->runner->runCommands([
+            'artisan cache:clear',
+            'artisan config:clear',
+            'artisan route:clear',
+            'artisan twig:clean',
+        ]);
+    }
+
+    /**
+     * Scan annotations
+     */
+    protected function annotations()
+    {
+        $this->runner->runCommands([
+            'artisan route:scan',
+            'artisan model:scan',
+        ]);
+    }
+
+    /**
+     * Optimize the application
+     */
+    protected function optimize()
+    {
+        $this->runner->runCommands([
+            'artisan config:cache',
+            'artisan route:cache',
+            'artisan config:cache',
+            'artisan optimize',
+        ]);
+    }
+
+    /**
+     * Update the database
+     */
+    protected function database()
+    {
+        $this->runner->runCommands([
+            'artisan migrate --force',
+            'artisan db:backup',
+        ]);
+    }
+
+    //////////////////////////////////////////////////////////////////////
+    ////////////////////////////// HELPERS ///////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+
+    /**
+     * Setup composer if necessary
+     */
+    private function getComposer()
+    {
+        $this->runner->runCommands([
+            'php -r "readfile(\'https://getcomposer.org/installer\');" > composer-setup.php',
+            'php composer-setup.php',
+            'php -r "unlink(\'composer-setup.php\');"',
+        ]);
+    }
+}
